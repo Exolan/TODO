830cf36154acb516c71f4a623e09308383552cdb
Exolan <97833724+Exolan@users.noreply.github.com>
2022-03-21 21:56:31 +0300

    todo210322

--- main.js
+++ main.js
@@ -24,6 +24,7 @@
     buttonAdd.id='buttonAdd'
     buttonAdd.innerText='Добавить дело'
     buttonAdd.type='button'
+    buttonAdd.style.backgroundColor="rgb(120,219,226)"
     buttonAdd.addEventListener('click', createTodoItem)
     form.append(buttonAdd)
   }
@@ -44,25 +45,45 @@
     li.className = 'collection-item'
     li.id='id'
     li.style="list-style-type: none"
-    li.textContent=input.value
+    
+
+    const div = document.createElement("div")
+    div.className="divElements"
+    li.append(div)
+    
+    const p = document.createElement('p')
+    p.className="text"
+    p.textContent=input.value
+    div.append(p)
 
     const buttons = document.createElement('div')
     buttons.className='buttons'
-    li.append(buttons)
+    div.append(buttons)
 
     let buttonDone=document.createElement('button')
     buttonDone.type='button'
     buttonDone.className='class="waves-effect waves-light btn-small'
     buttonDone.id='buttonDone'
     buttonDone.textContent='Выполнено'
+    buttonDone.style.backgroundColor="rgb(120,219,226)"
     buttonDone.addEventListener('click',()=>{
-      li.style.backgroundColor = 'green'
+      if(buttonDone.textContent=="Выполнено"){
+        buttonDone.style.backgroundColor="rgb(66,133,180)"
+        buttonDone.textContent='Отмена'
+        li.style.backgroundColor = 'rgb(68,148,74)'
+      }
+      else if(buttonDone.textContent=='Отмена'){
+        li.style.backgroundColor = ''
+        buttonDone.style.backgroundColor="rgb(120,219,226)"
+        buttonDone.textContent='Выполнено'
+      }
     })
     buttons.append(buttonDone)
 
     let buttonDelet=document.createElement('button')
     buttonDelet.type='button'
     buttonDelet.className='class="waves-effect waves-light btn-small'
+    buttonDelet.style.backgroundColor="rgb(238,32,77)"
     buttonDelet.id='buttonDelet'
     buttonDelet.textContent='Удалить'
     buttonDelet.addEventListener('click', ()=>{
